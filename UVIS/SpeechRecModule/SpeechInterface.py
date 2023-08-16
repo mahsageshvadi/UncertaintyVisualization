bc397c75b92a9f500d88ed030bb8fe8eb81a366a
Mahsa Geshvadi <mahsa@mahsas-air.partners.org>
2023-08-16 16:08:42 -0400

    speech recognition in background added

--- UVIS/SpeechRecModule/SpeechInterface.py
+++ UVIS/SpeechRecModule/SpeechInterface.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
-import openai
+import time
+
 
 
 
@@ -26,3 +27,32 @@ def takeCommand():
             if command==0:
                     continue
         return command
+
+
+def callbackWhileListening(recognizer, audio):
+    try:
+
+        print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+def callbackWhileListening(recognizer, audio):
+    try:
+
+        print( recognizer.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Couldn't understand the audio")
+    except sr.RequestError as e:
+            pass
+        
+def takeCommandInbackground():
+
+
+    r = sr.Recognizer()
+    m = sr.Microphone()
+    with m as source:
+        r.adjust_for_ambient_noise(source)  
+
+    top_listening = r.listen_in_background(m, callbackWhileListening)
