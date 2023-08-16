5f4a7db7310e1716dc73e6fab304c8ebc6e81b05
Mahsa Geshvadi <mahsa@mahsas-air.partners.org>
2023-08-16 16:38:01 -0400

    take command in background

--- UVIS/SpeechRecModule/SpeechInterface.py
+++ UVIS/SpeechRecModule/SpeechInterface.py
@@ -15,7 +15,7 @@ def takeCommand():
 
 
             print("Listening...")
-            audio=r.listen(source)
+            audio=r.listen(source, timeout = 3)
 
             try:
                 command=r.recognize_google(audio,language='en-in')
@@ -32,27 +32,21 @@ def takeCommand():
 def callbackWhileListening(recognizer, audio):
     try:
 
-        print("Google Speech Recognition thinks you said " + recognizer.recognize_google(audio))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
-def callbackWhileListening(recognizer, audio):
-    try:
-
         print( recognizer.recognize_google(audio))
     except sr.UnknownValueError:
         print("Couldn't understand the audio")
     except sr.RequestError as e:
             pass
         
-def takeCommandInbackground():
+def takeCommandInBackground():
+	r = sr.Recognizer()
+	m = sr.Microphone()
+	with m as source:
+		r.adjust_for_ambient_noise(source)  
+	stop_listening = r.listen_in_background(m, callbackWhileListening)
+	for _ in range(50): time.sleep(0.1)
+	stop_listening(wait_for_stop=False)
+	while True: 
+		time.sleep(0.1) 
 
-
-    r = sr.Recognizer()
-    m = sr.Microphone()
-    with m as source:
-        r.adjust_for_ambient_noise(source)  
-
-    top_listening = r.listen_in_background(m, callbackWhileListening)
+takeCommand()
