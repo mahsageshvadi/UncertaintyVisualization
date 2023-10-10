255e6965030d1455dc3e1d916f9462966bff96b6
Mahsa Geshvadi <mahsa@Mahsas-MacBook-Air.local>
2023-10-10 15:11:31 -0400

    TextMode  file separated

---
+++ UVIS/UIVISLib/TexModeVisualization.py
@@ -0,0 +1,47 @@
+import slicer
+class TexModeVisualization():
+
+    def __init__(self, uncertaintyArray):
+
+        self.markupsNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsFiducialNode")
+        self.markupsNode.AddControlPoint([0, 0, 0])
+        self.markupsNode.SetDisplayVisibility(False)
+        self.markupsNode.GetDisplayNode().SetUseGlyphScale(0)
+        self.markupsNode.GetDisplayNode().SetGlyphType(3)
+        self.markupsNode.GetDisplayNode().SetSelectedColor(0, 1, 0)
+        self.markupsNode.GetDisplayNode().SetActiveColor(0, 1, 0)
+        self.markupsNode.GetDisplayNode().SetTextScale(4)
+        self.uncertaintyArray = uncertaintyArray
+
+        self.isOn = False
+
+    def showMarkup(self, isChecked):
+
+        self.markupsNode.SetDisplayVisibility(isChecked)
+        self.isOn = isChecked
+
+    def changeGlyphType(self, index):
+
+        self.markupsNode.GetDisplayNode().SetGlyphType(index)
+        if index == 6:
+            self.markupsNode.GetDisplayNode().SetOpacity(0.3)
+
+        else:
+            self.markupsNode.GetDisplayNode().SetOpacity(1)
+
+    def moveMarkup(self, ras, point_Ijk):
+
+        if self.isOn:
+            try:
+
+                self.markupsNode.SetNthControlPointLabel(0, u"\u00B1 " + str(
+                    round(self.uncertaintyArray[point_Ijk[2]][point_Ijk[1]][point_Ijk[0]])) + " mm")
+                self.markupsNode.SetNthControlPointPosition(0, ras[0], ras[1], ras[2])
+                self.markupsNode.GetDisplayNode().SetGlyphSize(
+                    round(self.uncertaintyArray[point_Ijk[2]][point_Ijk[1]][point_Ijk[0]], 2))
+
+            except Exception as e:
+                pass
+
+    #   self.markupsNode.GetDisplayNode().SetViewNodeIDs(["vtkMRMLSliceNodeRed"])
+
--- UVIS/UVIS.py
+++ UVIS/UVIS.py
@@ -14,6 +14,7 @@ import imp
 
 from UIVISLib.UncertaintyForegroundVisualization import UncertaintyForegroundVisualization
 from UIVISLib.BackgroundModifiedVisualization import BackgroundModifiedVisualization
+from UIVISLib.TexModeVisualization import TexModeVisualization
 from pygame import mixer
 from slicer.ScriptedLoadableModule import *
 from slicer.util import VTKObservationMixin
@@ -68,7 +69,7 @@ class UVISWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
         logging.debug("Reloading UIVISLib")
 
         packageName='UIVISLib'
-        submoduleNames=['UncertaintyForegroundVisualization', 'BackgroundModifiedVisualization']
+        submoduleNames=['UncertaintyForegroundVisualization', 'BackgroundModifiedVisualization', 'TexModeVisualization']
         for submoduleName in submoduleNames:
             print(packageName + '/' + submoduleName)
             print(os.getcwd())
@@ -865,63 +866,6 @@ class UVISLogic(ScriptedLoadableModuleLogic):
         self.audioMode.performAudioMode(point_Ijk)
 
         self.uncertaintyForeground.performFlicker(point_Ijk)
-
-            
-
-    
-
-
-
-
-
-class TexModeVisualization():
-
-
-    def __init__(self, uncertaintyArray):
-    
-    
-        self.markupsNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLMarkupsFiducialNode")
-        self.markupsNode.AddControlPoint([0,0,0])
-        self.markupsNode.SetDisplayVisibility(False)
-        self.markupsNode.GetDisplayNode().SetUseGlyphScale(0)
-        self.markupsNode.GetDisplayNode().SetGlyphType(3)
-        self.markupsNode.GetDisplayNode().SetSelectedColor(0,1,0)
-        self.markupsNode.GetDisplayNode().SetActiveColor(0,1,0)
-        self.markupsNode.GetDisplayNode().SetTextScale(4)
-        self.uncertaintyArray = uncertaintyArray
-        
-        self.isOn = False
-
-        
-    def showMarkup(self, isChecked):
-     
-            self.markupsNode.SetDisplayVisibility(isChecked)
-            self.isOn = isChecked
-
-    def changeGlyphType(self, index):
-                
-                self.markupsNode.GetDisplayNode().SetGlyphType(index)
-                if index == 6:
-                    self.markupsNode.GetDisplayNode().SetOpacity(0.3)
-
-                else:
-                    self.markupsNode.GetDisplayNode().SetOpacity(1)
-            
-    def moveMarkup(self, ras, point_Ijk):
-            
-            if self.isOn:
-                try:
-                
-                    self.markupsNode.SetNthControlPointLabel(0,  u"\u00B1 " +  str(round(self.uncertaintyArray[point_Ijk[2]][point_Ijk[1]][point_Ijk[0]])) + " mm" )
-                    self.markupsNode.SetNthControlPointPosition(0, ras[0], ras[1], ras[2])
-                    self.markupsNode.GetDisplayNode().SetGlyphSize(round(self.uncertaintyArray[point_Ijk[2]][point_Ijk[1]][point_Ijk[0]], 2))
-                
-                except Exception as e:
-                    pass
-                    
-        
-     #   self.markupsNode.GetDisplayNode().SetViewNodeIDs(["vtkMRMLSliceNodeRed"])
-   
 
 class AudioMode():
     
