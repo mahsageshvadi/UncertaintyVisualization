46805aa807c356215e9069b78ce433216493c451
mahsa <mahsa.geshvadi001@umb.edu>
2024-01-09 09:45:29 -0500

    readme updated

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-Settings for different settings:
+Uncertainty Visualization Tool
+Overview
+This tool is designed for visualizing 3D uncertainty quantification values, offering a range of visualization techniques to suit different needs.
 
-1) Before using the module we calculate volumes for different fileters like Noise, Blur and Transparancy. Before use The user should play with filter_levels to get the best visualization.
+Features
+Compatible with 3D uncertainty data.
+Includes various filters like Noise, Blur, and Transparency for enhanced visualization.
+Interactive mining game for evaluating visualization methods.
+Getting Started
+Load 3D Uncertainty Data: Essential for the tool's functionality.
+Adjust Filters: Experiment with filter levels for optimal visualization.
+Explore Visualizations: Use the mining game to determine the most effective visualization technique.
+Why Choose This Tool?
+Customizable and user-friendly.
+Innovative approach to data visualization.
--- UVIS/UIVISLib/utils.py
+++ UVIS/UIVISLib/utils.py
@@ -3,7 +3,7 @@ import re
 
 _Extention_name = 'UVIS'
 _filter_types = ['Light', 'Noise', 'Blur']
-_filter_levels = [0,1, 2, 3, 4, 5, 6, 7, 8, 9]
+_filter_levels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 _noise_filter_local_scale = 15
 _blur_filter_local_scale = 0.5
@@ -11,29 +11,28 @@ _transparency_local_scale = 0.1
 
 
 def get_noise_filter_local_scale():
-
     return _noise_filter_local_scale
 
-def get_blur_filter_local_scale(filter_level):
 
-    return 0.5 * (1 + (filter_level* 0.5))
+def get_blur_filter_local_scale(filter_level):
+    return 0.5 * (1 + (filter_level * 0.5))
+
 
 def get_transparency_local_scale():
-
     return _transparency_local_scale
 
-def get_filter_types():
 
+def get_filter_types():
     return _filter_types
 
-def get_filter_levels():
 
+def get_filter_levels():
     return _filter_levels
 
+
 def get_project_root():
-    #c = slicer.util.getModule(_Extention_name).path
-    project_main_file_path ='Users/mahsa/BWH/Silcer/Uncertainty_VIS/UVIS'
+    # c = slicer.util.getModule(_Extention_name).path
+    project_main_file_path = 'Users/mahsa/BWH/Silcer/Uncertainty_VIS/UVIS'
     last_slash_index = project_main_file_path.rfind('/')
 
     return project_main_file_path[:last_slash_index + 1]
-
